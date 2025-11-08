05648233646f5094f9fca3e419fca1f18fda8e51
iagoramon-max <iagoramon@gmail.com>
2025-11-08 11:25:24 -0300

    Update celery.py

--- anna_beauty/celery.py
+++ anna_beauty/celery.py
@@ -1,1 +1,13 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "anna_beauty.settings")
+
+app = Celery("anna_beauty")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f"🔧 Celery está rodando direitinho, miga! Request: {self.request!r}")
 
