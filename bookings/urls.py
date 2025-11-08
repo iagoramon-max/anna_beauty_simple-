e2e3f79caaa8e2511b363cf3678db097bd35dbbc
iagoramon-max <iagoramon@gmail.com>
2025-11-08 12:35:03 -0300

    Update urls.py

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = "bookings"
-
 urlpatterns = [
-    path("", views.home, name="home"),
     path("book/", views.book_view, name="book"),
-    path("confirmation/", views.confirmation, name="confirmation"),
-    path("minhas-reservas/", views.my_appointments, name="my_appointments"),
+    path("confirmation/", views.confirmation_view, name="confirmation"),
+    path("my-appointments/", views.my_appointments_view, name="my_appointments"),
 ]
-
