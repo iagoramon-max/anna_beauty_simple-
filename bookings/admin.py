3b49e26c359017a59d999c3f1978e0347faabed1
iagoramon-max <iagoramon@gmail.com>
2025-11-08 12:34:46 -0300

    Update admin.py

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,1 +1,17 @@
+from django.contrib import admin
+from .models import Service, Client, Appointment
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("name", "phone", "email")
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ("client", "service", "start_datetime")
+    list_filter = ("service",)
+    search_fields = ("client__name", "client__phone")
 
