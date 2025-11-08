b8ba30ed5a417424b7680f21df5c36abb41eb827
iagoramon-max <iagoramon@gmail.com>
2025-11-08 11:20:12 -0300

    Update tasks.py

--- bookings/tasks.py
+++ bookings/tasks.py
@@ -1,1 +1,12 @@
+from celery import shared_task
+from .models import Appointment
+from .utils import send_whatsapp_confirmation
+
+@shared_task
+def send_whatsapp_confirmation_task(appointment_id):
+    try:
+        appointment = Appointment.objects.get(id=appointment_id)
+        send_whatsapp_confirmation(appointment)
+    except Appointment.DoesNotExist:
+        print("💔 Agendamento não encontrado, gatinha.")
 
